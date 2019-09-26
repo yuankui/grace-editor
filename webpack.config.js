d852fc43554b56e1320c554eaa22df51728e7610
yuankui <yuankui@meituan.com>
2019-09-26 20:37:32 +0800

    up

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ const merge = require('webpack-merge');
 const common = require('./config/webpack.common');
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
+require("babel-polyfill");
 
 module.exports = {
-  entry: {
-    renderer: path.join(__dirname, "src/index.js")
-  },
-  output: {
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
-  devServer: {
-    port: 8089,
-  }
+    entry: {
+        renderer: [
+            "babel-polyfill",
+            path.join(__dirname, "src/index.js"),
+        ]
+    },
+    output: {},
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    devServer: {
+        port: 8089,
+    }
 };
 
 module.exports = merge(common, module.exports);
