cedddc0e1d78aaf2e80e265276695107d87688da
yuankui <yuankui@meituan.com>
2019-09-29 17:12:55 +0800

    hide on select

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -105,7 +105,10 @@ class App extends React.Component<AppProps, AppState> {
                     footer={null}
                     onCancel={e => this.hideSearch()}
                 >
-                    <DropdownSelect ref={this.searchRef} onSelect={(i) => {console.log('select index:', i)}}
+                    <DropdownSelect ref={this.searchRef} onSelect={(i) => {
+                        console.log('select index:', i);
+                        this.toggleSearch();
+                    }}
                                     onSearch={keyword => this.onSearch(keyword)}
                     />
                 </Modal>
