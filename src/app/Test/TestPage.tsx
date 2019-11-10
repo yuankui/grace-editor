193a6dffb1dfdc138832f73187d96288ba468ca1
yuankui <yuankui64@gmail.com>
2019-11-10 17:40:41 +0800

    add page

--- src/app/Test/TestPage.tsx
+++ src/app/Test/TestPage.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-export default class TestPage extends React.Component {
+import {Editor} from 'slate-react'
+import {Value} from 'slate';
+import './TestPage.less';
+
+const initialValue = Value.fromJSON({
+    document: {
+        nodes: [
+            {
+                object: 'block',
+                type: 'paragraph',
+                nodes: [
+                    {
+                        object: 'text',
+                        text: 'A line of text in a paragraph.',
+                    },
+                ],
+            },
+        ],
+    },
+});
+
+interface State {
+    value: any,
+}
+
+export default class TestPage extends React.Component<any, State> {
+    state = {
+        value: initialValue,
+    };
+
+    // On change, update the app's React state with the new editor value.
+    onChange = ({value}) => {
+        this.setState({value})
+    };
+
     render() {
-        return <h1>hello</h1>;
+        return <div className='test-container'>
+            <Editor value={this.state.value} onChange={this.onChange}/>
+        </div>
     }
 }
