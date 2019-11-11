e83563794ec0de5d1b09753e44ed6626bd5bc330
yuankui <yuankui@meituan.com>
2019-11-11 13:50:34 +0800

    add progress bar

--- src/redux/commands/git/GitPushCommand.ts
+++ src/redux/commands/git/GitPushCommand.ts
@@ -2,6 +2,10 @@ import {CommandType} from "../index";
 import {AppStore} from "../../store";
 import GitCommand from "./GitCommand";
 import {message} from "antd";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+});
 
 export default class GitPushCommand extends GitCommand {
     name(): CommandType {
@@ -11,7 +15,9 @@ export default class GitPushCommand extends GitCommand {
     async processGit(state: AppStore): Promise<AppStore> {
         if (state.repo) {
             await state.repo.add('.');
+            NProgress.start();
             const log = await state.repo.push('origin', 'master');
+            NProgress.done();
             message.info("push success");
         }
 
