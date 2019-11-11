6f053b815e021dce5be5f15531805b75780f934c
yuankui <yuankui@meituan.com>
2019-11-11 15:48:44 +0800

    add code  block
    todo: insert new block

---
+++ src/app/renders/Slatejs/plugins/code/CodePlugin.tsx
@@ -0,0 +1,33 @@
+import {Plugin} from 'slate-react';
+import React from "react";
+import ToggleBlockOnPrefix from "../common";
+import isHotkey from "is-hotkey";
+
+const CodeBlock = 'code-block';
+
+export function createCodePlugin(): Plugin {
+    return {
+        onKeyDown: (event, editor, next) => {
+            if (ToggleBlockOnPrefix('^', event, editor, e => {
+                e.setBlocks(CodeBlock);
+            })) return;
+
+            if (isHotkey('enter', event.nativeEvent)) {
+                if (editor.value.focusBlock.type == CodeBlock) {
+                    editor.insertText("\n");
+                    event.preventDefault();
+                    return;
+                }
+            }
+            next();
+        },
+
+        renderBlock: (props, editor, next) => {
+            if (props.node.type == CodeBlock) {
+                return <pre className={CodeBlock} {...props.attributes}>{props.children}</pre>
+            } else {
+                return next();
+            }
+        },
+    }
+}
--- src/app/renders/Slatejs/plugins/plugins.ts
+++ src/app/renders/Slatejs/plugins/plugins.ts
@@ -2,11 +2,13 @@ import {Plugin} from 'slate';
 import {createHeaderPlugin} from "./header/HeaderPlugin";
 import {createListPlugin} from "./list/ListPlugin";
 import {createTodoPlugin} from "./todo/TodoPlugin";
+import {createCodePlugin} from "./code/CodePlugin";
 
 export default function createSlateEditorPlugins(): Array<Plugin> {
     return [
         createHeaderPlugin(),
         createListPlugin(),
         createTodoPlugin(),
+        createCodePlugin(),
     ];
 }
