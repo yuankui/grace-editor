b0565364d6e0903cddce437f65e4f4490248eb21
yuankui <yuankui@meituan.com>
2019-11-21 21:15:26 +0800

    use functional programming

---
+++ src/app/post/PostTitle.tsx
@@ -0,0 +1,70 @@
+import {AppStore, Post} from "../../redux/store";
+import React from "react";
+import {If, MaterialIcon} from "../../utils";
+import Point from "../../icons";
+import {OperationButton} from "../../common/OperationButton";
+import {Popover} from "antd";
+import {useDispatch, useStore} from "react-redux";
+import {PostSelectCommand} from "../../redux/commands/menu/PostSelectCommand";
+import {ToggleExpandCommand} from "../../redux/commands/menu/ToggleExpandCommand";
+import {CreateNewPostCommand} from "../../redux/commands/CreateNewPostCommand";
+import {createPostId} from "../../redux/utils";
+import {DeletePostRecursiveCommand} from "../../redux/commands/DeletePostRecursiveCommand";
+
+interface Props {
+    post: Post,
+    expanded: boolean,
+}
+
+export const PostTitle: React.FC<Props> = props => {
+    const {post: item, expanded, } = props;
+    const {id: postId} = item;
+    const state: AppStore = useStore<AppStore>().getState();
+    const dispatch = useDispatch<any>();
+    const menu = (
+        <ul className='actions'>
+            <li>
+                <a onClick={event => {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    dispatch(new DeletePostRecursiveCommand(item.id));
+                }}>删除</a>
+            </li>
+        </ul>
+    );
+
+    const childrenIds = state.posts.childrenMap.get(item.id);
+    const hasChildren = childrenIds != null && childrenIds.length != 0;
+
+    return <div
+        onClick={() => dispatch(new PostSelectCommand(item.id))}
+        onDoubleClick={(e) => {
+            dispatch(new ToggleExpandCommand(postId));
+            e.preventDefault();
+            e.stopPropagation();
+        }}>
+            <span className='title-prefix'>
+                <If test={hasChildren}>
+                    <span className={'expand-button' + ' expanded-' + expanded}
+                          onClick={e => dispatch(new ToggleExpandCommand(item.id))}>
+                        <MaterialIcon value="play_arrow"/>
+                    </span>
+                </If>
+                <If test={!hasChildren}>
+                    <Point/>
+                </If>
+            </span>
+        <span className='title'>
+                {item.title == "" ? "未命名" : item.title}
+            </span>
+        <OperationButton onClick={() => dispatch(new CreateNewPostCommand(createPostId(), item.id))}>
+            <MaterialIcon value='add'/>
+        </OperationButton>
+
+        <Popover content={menu} trigger="click" placement='bottom'>
+            <OperationButton>
+                <MaterialIcon value='more_horiz'/>
+            </OperationButton>
+        </Popover>
+    </div>;
+};
--- src/app/post/PostTree.tsx
+++ src/app/post/PostTree.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import {If, mapState, MaterialIcon} from "../../utils";
+import {If, mapState} from "../../utils";
 import {connect} from "react-redux";
 import {AppStore, Post} from "../../redux/store";
 import Collapse from "./Collapse";
-import {OperationButton} from "../../common/OperationButton";
-import {Popover} from "antd";
-import {createPostId} from "../../redux/utils";
-import {CreateNewPostCommand} from "../../redux/commands/CreateNewPostCommand";
-import {PostSelectCommand} from "../../redux/commands/menu/PostSelectCommand";
-import {DeletePostRecursiveCommand} from "../../redux/commands/DeletePostRecursiveCommand";
 import {Dispatch} from "redux";
-import {ToggleExpandCommand} from "../../redux/commands/menu/ToggleExpandCommand";
-import Point from "../../icons";
+import {PostTitle} from "./PostTitle";
 
 interface Props {
     postId: string,
@@ -44,66 +37,8 @@ class PostTree extends React.Component<Props> {
         </Collapse>;
     }
 
-    openPost() {
-        this.props.dispatch(new PostSelectCommand(this.props.postId));
-    }
     renderTitle(item: Post, expanded: boolean) {
-        const menu = (
-            <ul className='actions'>
-                <li>
-                    <a onClick={event => this.delete(event, item.id)}>删除</a>
-                </li>
-            </ul>
-        );
-
-        const childrenIds = this.props.state.posts.childrenMap.get(item.id);
-        const hasChildren = childrenIds != null && childrenIds.length != 0;
-
-        return (<div onClick={() => this.openPost()} onDoubleClick={(e) => this.doubleClick(item, e)}>
-            <span className='title-prefix'>
-                <If test={hasChildren}>
-                    <span className={'expand-button' + ' expanded-' + expanded}
-                          onClick={e => this.onExpandKey(item.id)}>
-                        <MaterialIcon value="play_arrow"/>
-                    </span>
-                </If>
-                <If test={!hasChildren}>
-                    <Point/>
-                </If>
-            </span>
-            <span className='title'>
-                {item.title == "" ? "未命名" : item.title}
-            </span>
-            <OperationButton onClick={() => this.createNewPost(item.id)}>
-                <MaterialIcon value='add'/>
-            </OperationButton>
-
-            <Popover content={menu} trigger="click" placement='bottom'>
-                <OperationButton>
-                    <MaterialIcon value='more_horiz'/>
-                </OperationButton>
-            </Popover>
-        </div>);
-    }
-
-    onExpandKey(key: string) {
-        this.props.dispatch(new ToggleExpandCommand(key));
-    }
-
-    doubleClick = (item: Post, e: React.MouseEvent<HTMLSpanElement>) => {
-        this.onExpandKey(item.id);
-        e.stopPropagation();
-    };
-
-    createNewPost = (parentId: string | null) => {
-        const postId = createPostId();
-        this.props.dispatch(new CreateNewPostCommand(postId, parentId));
-        this.props.dispatch(new PostSelectCommand(postId));
-    };
-
-    delete(e: React.MouseEvent<HTMLAnchorElement>, postId: string) {
-        e.stopPropagation();
-        this.props.dispatch(new DeletePostRecursiveCommand(postId));
+        return <PostTitle post={item} expanded={expanded}/>
     }
 }
 
