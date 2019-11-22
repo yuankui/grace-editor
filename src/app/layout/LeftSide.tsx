9c71b00f6c50c03a24ce925637ddd91f5f4202ee
yuankui <yuankui64@gmail.com>
2019-11-23 03:20:54 +0800

    update resizable

--- src/app/layout/LeftSide.tsx
+++ src/app/layout/LeftSide.tsx
@@ -1,9 +1,29 @@
 import {SiderMenu} from "./left/SiderMenu";
-import React from "react";
+import React, {useState} from "react";
+import {Resizable} from "re-resizable";
 
 export const LeftSide: React.FC = () => {
-    return <div className='app-left-side'>
+    const [width, setWidth] = useState(300);
+    const [delta, setDelta] = useState(0);
+
+    const accWidth = width + delta;
+    return <Resizable enable={{right: true}}
+                      style={{flex: `0 0 ${accWidth}px`}}
+                      className='app-left-side'
+                      handleClasses={{right: 'resize-handle'}}
+                      handleStyles={{right: {width: 5, backgroundColor: 'blue'}}}
+                      onResizeStop={() => {
+                          setWidth(width + delta);
+                          setDelta(0);
+                      }}
+                      onResize={(event, direction, elementRef, d) => {
+                          setDelta(d.width);
+                      }}
+                      onResizeStart={() => {
+                          setDelta(0);
+                      }}
+    >
         <div className='app-left-handle'/>
         <SiderMenu/>
-    </div>
+    </Resizable>;
 };
