0b8efb397cbb4fe88616d885b4426a5771bb8739
yuankui <yuankui64@gmail.com>
2019-11-23 21:47:28 +0800

    style nav disabled

--- src/app/layout/right/TopBar/Nav.tsx
+++ src/app/layout/right/TopBar/Nav.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import {Icon} from "antd";
 import {BreadCrumb} from "./BreadCrumb";
 import {useHistory} from 'react-router';
+import {MemoryHistory} from 'history';
 
 export const Nav: React.FC = () => {
 
-    const history = useHistory();
+    const history = useHistory() as MemoryHistory;
 
-    console.log(history);
+    const cannotBack = history.index == 0;
+    const cannotForward = history.index >= history.entries.length - 1;
+
     return <div className='nav'>
-        <a className={''} onClick={() => history.goBack()}><Icon type="arrow-left" /></a>
-        <a onClick={() => history.goForward()}><Icon type="arrow-right" /></a>
+        <a className={'disable-' + cannotBack} onClick={() => history.goBack()}><Icon type="arrow-left" /></a>
+        <a className={"disable-" + cannotForward} onClick={() => history.goForward()}><Icon type="arrow-right" /></a>
         <BreadCrumb/>
     </div>
 };
