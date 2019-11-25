41486563aabe9691c6904c85b73b3de71b7a507c
yuankui <yuankui@meituan.com>
2019-11-25 11:20:19 +0800

    offline google fonts & material-icon

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,6 +8,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
                 test: /\.less$/,
                 use: [{
                     loader: 'style-loader' // creates style nodes from JS strings
