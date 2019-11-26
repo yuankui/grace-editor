a565ed9f224ae36c10d7403feb17857ff4af4972
yuankui <yuankui@meituan.com>
2019-11-26 12:09:27 +0800

    init workspace if not

--- src/redux/commands/git/GitSetupCommand.ts
+++ src/redux/commands/git/GitSetupCommand.ts
@@ -1,16 +1,29 @@
 import {AppCommand, CommandType} from "../index";
 import {AppStore} from "../../store";
 import simplegit from "../../../copies/simple-git/promise";
+import {Mapper} from "redux-commands";
+import FileSystem from "../../../backend/electron/FileSystem";
+import GitInitCommand from "./GitInitCommand";
 
 export default class GitSetupCommand extends AppCommand {
     name(): CommandType {
         return "Git/Setup";
     }
 
-    process(state: AppStore): AppStore {
-        return {
-            ...state,
-            repo: simplegit(state.settings.workSpace),
+    async process(s: AppStore): Promise<Mapper<AppStore>> {
+        const fs = new FileSystem();
+        const workSpace = s.settings.workSpace;
+
+        const stats = await fs.stats(workSpace);
+        if (!stats.isDirectory()) {
+            await fs.mkdir(workSpace);
+            await new GitInitCommand().process(s);
+        }
+        return (state) => {
+            return {
+                ...state,
+                repo: simplegit(state.settings.workSpace),
+            }
         }
     }
 
