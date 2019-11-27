2a772c5ad8c8bc34b9426f47728eab02afa3cb1e
yuankui <yuankui64@gmail.com>
2019-11-27 23:39:40 +0800

    add

--- src/redux/commands/git/GitPullCommand.ts
+++ src/redux/commands/git/GitPullCommand.ts
@@ -1,33 +1,30 @@
-import {CommandType} from "../index";
+import {AppCommand, CommandType} from "../index";
 import {AppStore} from "../../store";
-import GitCommand from "./GitCommand";
 import {message} from "antd";
 import NProgress from 'nprogress';
+import {Dispatch} from "redux";
 
-NProgress.configure({
-});
+NProgress.configure({});
 
-export default class GitPullCommand extends GitCommand {
+export default class GitPullCommand extends AppCommand {
     name(): CommandType {
         return "Git/Pull";
     }
 
-    async processGit(state: AppStore): Promise<AppStore> {
-        if (state.repo) {
-            NProgress.start();
-            try {
-                await state.repo.pull('origin', 'master');
-                message.info("pull success");
-                NProgress.done();
-            } catch (e) {
-                message.error(e.toString());
-                NProgress.done();
-            }
-
-
+    async process(state: AppStore, dispatch: Dispatch<any>): Promise<void> {
+        if (!state.repo) {
+            return;
         }
 
-        return state;
+        NProgress.start();
+        try {
+            await state.repo.pull('origin', 'master');
+            message.info("pull success");
+            NProgress.done();
+        } catch (e) {
+            message.error(e.toString());
+            NProgress.done();
+        }
     }
 
 }
