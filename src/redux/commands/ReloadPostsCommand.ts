2fb686e68567c36fd493e7ba1eb0518aec9d2adf
yuankui <yuankui64@gmail.com>
2019-11-27 23:44:13 +0800

    add

--- src/redux/commands/ReloadPostsCommand.ts
+++ src/redux/commands/ReloadPostsCommand.ts
@@ -1,28 +1,27 @@
 import {AppCommand, CommandType} from "./index";
 import {AppStore, PostsStore} from "../store";
 import {buildPostTree} from "../utils";
-import {Mapper} from "redux-commands";
+import {Dispatch} from "redux";
+import {UpdateStateCommand} from "./UpdateStateCommand";
 
 export class ReloadPostsCommand extends AppCommand {
     name(): CommandType {
         return "ReloadPosts";
     }
 
-    async process(state: AppStore): Promise<Mapper<AppStore>> {
+    async process(state: AppStore, dispatch: Dispatch<any>): Promise<void> {
         const startTime = new Date().getUTCMilliseconds();
         let posts = await state.backend.getPosts();
-        return (s: AppStore): AppStore => {
-            let postsStore: PostsStore = buildPostTree(posts);
-            const cost = new Date().getUTCMilliseconds() - startTime;
-
-            console.log('reload cost:', cost);
-            return {
-                ...s,
-                posts: {
-                    ...postsStore,
-                }
+        let postsStore: PostsStore = buildPostTree(posts);
+        const cost = new Date().getUTCMilliseconds() - startTime;
+        console.log('load cost:', cost);
+        const newState = {
+            posts: {
+                ...postsStore,
             }
-        }
+        };
+
+        dispatch(new UpdateStateCommand(newState));
     }
 
 }
