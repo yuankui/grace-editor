9604e5f17a239e2605752fcb908f71290b702e19
yuankui <yuankui64@gmail.com>
2019-11-28 00:34:44 +0800

    up

--- src/redux/commands/app/AppInitCommand.ts
+++ src/redux/commands/app/AppInitCommand.ts
@@ -5,6 +5,7 @@ import ReloadSettingsCommand from "../ReloadSettingsCommand";
 import GitSetupCommand from "../git/GitSetupCommand";
 import {InitBackendCommand} from "../InitBackendCommand";
 import {ReloadPostsCommand} from "../ReloadPostsCommand";
+import {CheckRemoteCommand} from "./CheckRemoteCommand";
 
 export class AppInitCommand extends AppCommand {
     name(): CommandType {
@@ -16,6 +17,7 @@ export class AppInitCommand extends AppCommand {
         await dispatch(new GitSetupCommand());
         await dispatch(new InitBackendCommand());
         await dispatch(new ReloadPostsCommand());
+        await dispatch(new CheckRemoteCommand());
     }
 
 }
