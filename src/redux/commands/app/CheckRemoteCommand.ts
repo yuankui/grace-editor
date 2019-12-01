b3a14d8b221a53eb422b469c8575ad5513fc38f4
yuankui <yuankui64@gmail.com>
2019-12-01 12:26:01 +0800

    up

--- src/redux/commands/app/CheckRemoteCommand.ts
+++ src/redux/commands/app/CheckRemoteCommand.ts
@@ -13,32 +13,36 @@ export class CheckRemoteCommand extends AppCommand {
             return;
         }
 
-        const remotes = await state.repo.getRemotes(true);
-        for (let remote of remotes) {
-            if (remote.name === 'origin') {
-                const status: AppStatus = {
-                    canGitPush: remote.refs.push != null,
-                    canGitPull: remote.refs.fetch != null,
-                };
+        try {
+            const remotes = await state.repo.getRemotes(true);
+            for (let remote of remotes) {
+                if (remote.name === 'origin') {
+                    const status: AppStatus = {
+                        canGitPush: remote.refs.push != null,
+                        canGitPull: remote.refs.fetch != null,
+                    };
 
-                if (state.status.canGitPull != status.canGitPull
-                    && state.status.canGitPush != status.canGitPush) {
-                    await dispatch(new UpdateStateCommand({
-                        status: status,
-                    }));
+                    if (state.status.canGitPull != status.canGitPull
+                        && state.status.canGitPush != status.canGitPush) {
+                        await dispatch(new UpdateStateCommand({
+                            status: status,
+                        }));
+                    }
+                    return;
                 }
+            }
+
+            if (state.status.canGitPull || state.status.canGitPush) {
+                await dispatch(new UpdateStateCommand({
+                    status: {
+                        canGitPush: false,
+                        canGitPull: false,
+                    }
+                }));
                 return;
             }
-        }
+        } catch (e) {
 
-        if (state.status.canGitPull || state.status.canGitPush) {
-            await dispatch(new UpdateStateCommand({
-                status: {
-                    canGitPush: false,
-                    canGitPull: false,
-                }
-            }));
-            return;
         }
     }
 
