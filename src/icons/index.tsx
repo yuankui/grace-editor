dcd5025fc4551272df412d2395fa25ba00590023
yuankui <yuankui64@gmail.com>
2019-12-15 13:04:00 +0800

    title prefix theme

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Point() {
-    return <svg width="20" height="20" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" className="doc-icon">
+    return <svg width="20" height="20" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" className="post-prefix-point">
         <g id="point" fill="none" fillRule="evenodd">
             <circle id="\u692D\u5706\u5F62" fill="#000" cx="24" cy="24" r="6"/>
         </g>
