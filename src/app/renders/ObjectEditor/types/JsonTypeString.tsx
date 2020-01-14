2fa94e7b90bbf6787d5f2eb69fc02cfaeb03464b
yuankui <yuankui@meituan.com>
2020-01-14 14:17:51 +0800

    up

--- src/app/renders/ObjectEditor/types/JsonTypeString.tsx
+++ src/app/renders/ObjectEditor/types/JsonTypeString.tsx
@@ -1,16 +1,32 @@
 import _ from "lodash";
 import JsonType, {OnChange} from "./JsonType";
 import React, {ReactNode} from "react";
+import {Popover, message, Modal} from "antd";
+
+const {clipboard} = require('electron');
 
 export default class JsonTypeString implements JsonType {
     checkType(value: object): boolean {
         return _.isString(value);
     }
 
-    render(value: object, suffix: ReactNode, prefix: ReactNode ,onChange: OnChange) {
+    render(value: object, suffix: ReactNode, prefix: ReactNode, onChange: OnChange) {
+        const actions = <div>
+            <a onClick={e => {
+                e.stopPropagation();
+                e.preventDefault();
+                clipboard.writeText(value.toString());
+                message.info("copy success");
+            }}>复制</a>
+
+            <span> | </span>
+            <a>编辑</a>
+        </div>;
         return <span>
             {prefix}
-            <span className='json-type-string'>"{value}"</span>
+            <Popover content={actions} trigger={"click"} placement={'top'}>
+                <a className='json-type-string'>"{value}"</a>
+            </Popover>
             {suffix}
         </span>;
     }
