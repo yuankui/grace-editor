1a30ae6c587dc15b1f60f4e7a3ae6b55249fa881
yuankui <yuankui@meituan.com>
2020-01-20 18:48:52 +0800

    add handle

--- src/app/layout/LeftSide.tsx
+++ src/app/layout/LeftSide.tsx
@@ -11,6 +11,20 @@ export const LeftSide: React.FC = () => {
                       style={{flex: `0 0 ${accWidth}px`}}
                       className='app-left-side'
                       handleClasses={{right: 'resize-handle'}}
+                      handleComponent={
+                          {
+                              right: (<a onClick={() => {
+                              }}>
+                                  <svg viewBox="64 64 896 896" focusable="false" className="" data-icon="more"
+                                       width='24px'
+                                       height='24px'
+                                       fill="currentColor" aria-hidden="true">
+                                      <path
+                                          d="M456 231a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 280a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 280a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"/>
+                                  </svg>
+                              </a>)
+                          }
+                      }
                       handleStyles={{right: {width: 5}}}
                       minWidth={200}
                       maxWidth={500}
