6143661bdefc5471c2c3b98d00bfb58c346fcba0
yuankui <yuankui@meituan.com>
2020-01-23 16:36:39 +0800

    acc performance

--- src/app/renders/ObjectEditor/ObjectView/json/types/JsonTypeObject.tsx
+++ src/app/renders/ObjectEditor/ObjectView/json/types/JsonTypeObject.tsx
@@ -1,11 +1,25 @@
 import _ from "lodash";
-import JsonType from "./JsonType";
+import JsonType, {JsonTypeProps} from "./JsonType";
 import React from "react";
 import TypeFactory from "./TypeFactory";
 import JsonKey from "./JsonKey";
 import Suffix from "./JsonSuffix";
+import {If} from "../../../../../../utils";
+import {Icon} from "antd";
 
-export default class JsonTypeObject extends JsonType {
+interface State {
+    visible: boolean,
+}
+export default class JsonTypeObject extends JsonType<State> {
+
+
+    constructor(props: JsonTypeProps, context: any) {
+        super(props, context);
+        this.state = {
+            visible: true,
+        }
+    }
+
     checkType(value: object): boolean {
         return _.isPlainObject(value);
     }
@@ -17,7 +31,7 @@ export default class JsonTypeObject extends JsonType {
                 const [key, v] = entry;
                 const last = index == array.length - 1;
 
-                const suffix = last ? <Suffix/>: null;
+                const suffix = !last ? <Suffix/>: null;
                 const JsonType = TypeFactory(v);
                 const keyNode = <JsonKey value={key}/>;
 
@@ -32,10 +46,28 @@ export default class JsonTypeObject extends JsonType {
         return <div className='json-type-object'>
             <div>
                 {prefix}
-                {"{"}
+                {"{ "}
+                <a className={'expand-' + this.state.visible} onClick={() => {
+                    this.setState({
+                        visible: !this.state.visible,
+                    })
+                }}>
+                    <If test={this.state.visible}>
+                        <Icon type="minus-circle" />
+                    </If>
+                    <If test={!this.state.visible}>
+                        <Icon type="plus-circle" />
+                    </If>
+                </a>
+                <If test={!this.state.visible}>
+                    {" }"}
+                </If>
             </div>
-            {entries}
-            <div>{"}"}{suffix}</div>
+
+            <If test={this.state.visible}>
+                {entries}
+                <div>{"}"}{suffix}</div>
+            </If>
         </div>;
     }
 }
--- src/app/renders/ObjectEditor/ObjectView/json/types/Text.tsx
+++ src/app/renders/ObjectEditor/ObjectView/json/types/Text.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import isHotkey, {isKeyHotkey} from "is-hotkey";
 import {Controlled as CodeMirror} from "react-codemirror2";
 import 'codemirror/mode/javascript/javascript';
@@ -11,6 +11,7 @@ interface Props {
 const Text: React.FC<Props> = props => {
     const [editable, setEditable] = useState(false);
 
+
     if (!editable) {
         return <a className='json-type-string' onDoubleClick={e => {
             e.preventDefault();
@@ -19,26 +20,44 @@ const Text: React.FC<Props> = props => {
         }}>{JSON.stringify(props.text)}</a>
     }
 
+    return <Editable text={props.text}
+                     onChange={props.onChange}
+                     onDone={() => setEditable(false)}/>;
+};
+
+const Editable: React.FC<{ text, onDone, onChange }> = (props) => {
+    const [text, setText] = useState(props.text);
+
+    const sync = useMemo(() => {
+        let timer: any = null;
+        return text => {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                props.onChange(text);
+            }, 200);
+        }
+    }, []);
+
     return <div className='json-text'>
         <CodeMirror
-            value={props.text}
+            value={text}
             options={{
                 mode: 'javascript',
                 theme: 'monokai',
                 lineNumbers: true
             }}
-            onKeyDown={(editor, e:Event) => {
+            onKeyDown={(editor, e: Event) => {
                 if (isKeyHotkey('shift+enter', e as any)) {
-                    setEditable(false);
+                    props.onDone();
                 } else if (isHotkey('esc', e as any)) {
-                    setEditable(false);
+                    props.onDone();
                 }
             }}
-            onBlur={() => setEditable(false)}
+            onBlur={() => props.onDone()}
             onBeforeChange={(editor, data, value) => {
-                props.onChange(value);
+                setText(value);
+                sync(value);
             }}/>
     </div>;
 };
-
 export default Text;
