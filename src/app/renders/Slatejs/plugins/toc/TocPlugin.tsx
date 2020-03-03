98e1415f695561275891b47bfda8c76f0eef6402
yuankui <yuankui@yuankui-hackintosh.local>
2020-03-03 17:34:31 +0800

    update readonly

--- src/app/renders/Slatejs/plugins/toc/TocPlugin.tsx
+++ src/app/renders/Slatejs/plugins/toc/TocPlugin.tsx
@@ -7,6 +7,9 @@ import React from "react";
 export function createTocPlugin(): Plugin {
     return {
         renderEditor: (props, editor, next) => {
+            if (props.readOnly) {
+                return next();
+            }
             return <div style={{height: '100%'}} className='app-toc-container'>
                 <TableOfContent value={editor.value}/>
                 {next()}
