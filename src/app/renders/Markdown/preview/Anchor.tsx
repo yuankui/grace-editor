d2b6294d49040870152992732bf7f32406f9151a
yuankui <yuankui64@gmail.com>
2020-03-13 17:40:00 +0800

    support anchor open in browser

--- src/app/renders/Markdown/preview/Anchor.tsx
+++ src/app/renders/Markdown/preview/Anchor.tsx
@@ -2,17 +2,15 @@ import React, { FunctionComponent } from 'react';
 import open from 'open';
 
 interface Props {
-    props: {
-        title: string,
-        href: string,
-    },
+    title: string,
+    href: string,
 }
 
 const Anchor: FunctionComponent<Props> = (props) => {
-    return <a {...props.props} onClick={e => {
+    return <a {...props} onClick={e => {
         e.stopPropagation();
         e.preventDefault();
-        open(props.props.href);
+        open(props.href);
     }}>
         {props.children}
     </a>;
