478cc01cc6bc9ac9804b2ea7cc30f34ffe6eb3bc
yuankui <yuankui64@gmail.com>
2020-04-02 14:09:37 +0800

    update

--- src/backend/electron/uuid_test.js
+++ src/backend/electron/uuid_test.js
@@ -1,5 +1,15 @@
-const uuid = require('uuid/v4');
+const fs = require('fs');
+require('js-base64');
+const fd = fs.openSync('./FileSystem.ts', 'r');
 
-console.log(uuid());
-console.log(uuid());
-console.log(uuid());
+
+const buf = new Uint8Array(10000);
+const len = 100;
+fs.read(fd, buf, 0, len, 0, (err, bytesRead, buffer) => {
+
+    const read = buffer.subarray(0, 100);
+    const s = new TextDecoder("utf-8").decode(read);
+
+    console.log(s);
+});
+
