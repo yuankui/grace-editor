4d4b4d926cfedd1531cc47bcfbc417fe960381b9
yuankui <yuankui64@gmail.com>
2020-04-05 10:28:01 +0800

    add full-screen listener

--- src/globalPlugins/FocusMode/createFocusModePlugin.tsx
+++ src/globalPlugins/FocusMode/createFocusModePlugin.tsx
@@ -3,6 +3,10 @@ import {GlobalPlugin} from "../GlobalPlugin";
 export function createFocusModePlugin(): GlobalPlugin {
     return {
         init: context => {
+            context.electron.remote.getCurrentWindow()
+                .on('enter-full-screen', () => {
+                    context.setSetting('pluginId.FocusMode', 'on', true);
+                });
             context.registerHook({
                 containerId: 'app.more.settings',
                 priority: 0,
@@ -12,6 +16,9 @@ export function createFocusModePlugin(): GlobalPlugin {
                     // factory(title, value, onChange) => ReactNode
                     const value = context.getSetting("pluginId.FocusMode", "on");
                     return props.factory("FocusMode", value, (state) => {
+                        setTimeout(() => {
+                            context.electron.remote.getCurrentWindow().setFullScreen(!!state);
+                        }, 100);
                         context.setSetting("pluginId.FocusMode", "on", state);
                     })
                 }
