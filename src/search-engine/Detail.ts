db27c6377a70374e4272f94c6636c5dd9917c856
yuankui <yuankui64@gmail.com>
2020-04-08 14:19:17 +0800

    up

--- src/search-engine/Detail.ts
+++ src/search-engine/Detail.ts
@@ -1,6 +1,4 @@
 import RocksDB from "rocksdb";
-import {range} from "rxjs";
-import * as op from 'rxjs/operators';
 
 export class Detail<T = any> {
     private path: string;
@@ -41,6 +39,18 @@ export class Detail<T = any> {
         })
     }
 
+    async get(docId: string): Promise<T> {
+        return new Promise<any>((resolve, reject) => {
+            this.rocksdb.get(docId, (err, value) => {
+                if (err == null) {
+                    resolve(JSON.parse(value.toString()));
+                } else {
+                    reject(err);
+                }
+            })
+        })
+    }
+
     async remove(docId: string) {
         return new Promise((resolve, reject) => {
             this.rocksdb.del(docId, err => {
@@ -55,50 +65,31 @@ export class Detail<T = any> {
 
     async list(page: number, pageSize: number): Promise<Array<T>> {
         const iterator = this.rocksdb.iterator({});
-
-        return new Promise((resolve, reject) => {
-            range(0, page * pageSize)
-                .pipe(
-                    op.bufferCount(pageSize),
-                    op.flatMap((keys) => {
-                        const promises = keys.map(() => {
-                            return new Promise((res, rej) => {
-                                iterator.next(err => {
-                                    if (err == null) {
-                                        res();
-                                    } else {
-                                        rej(err);
-                                    }
-                                })
-                            })
-                        });
-                        return Promise.all(promises);
-                    }),
-                    op.last(),
-                )
-                .subscribe(() => {
-                    // skip done, start get
-                    range(0, pageSize)
-                        .pipe(
-                            op.flatMap((key) => {
-                                return new Promise<any>((resolve1, reject1) => {
-                                    iterator.next((err, key, value) => {
-                                        if (err == null) {
-                                            resolve1(JSON.parse(value.toString()))
-                                        } else {
-                                            reject1(err);
-                                        }
-                                    })
-                                })
-                            }),
-                            op.bufferCount(pageSize),
-                        )
-                        .subscribe(value => {
-                            resolve(value);
-                        }, error => {
-                            reject(error);
-                        }, )
+        const next = async (iter) => {
+            return new Promise<any>((resolve, reject) => {
+                iterator.next((err, key, value) => {
+                    if (err == null) {
+                        resolve([key, value]);
+                    } else {
+                        reject(err);
+                    }
                 })
-        })
+            })
+        };
+        // 1. skip
+        for (let i = 0; i < page * pageSize; i++) {
+            await next(iterator);
+        }
+
+        const array = <any>[];
+        // 2. get
+        for (let i = 0; i < pageSize; i++) {
+            const [key, value] = await next(iterator);
+            if (key == undefined && value == undefined) {
+                break;
+            }
+            array.push(JSON.parse(value.toString()));
+        }
+        return array;
     }
 }
