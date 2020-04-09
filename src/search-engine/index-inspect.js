d6c129ffd6e9fc0d5b8a091c110f6ec8a236a886
yuankui <yuankui64@gmail.com>
2020-04-10 00:15:54 +0800

    TODO: remove old reverse index where updating

--- src/search-engine/index-inspect.js
+++ src/search-engine/index-inspect.js
@@ -1,3 +1,5 @@
+const {RoaringBitmap32} = require("roaring");
+
 const RocksDB = require("rocksdb");
 
 
@@ -13,34 +15,57 @@ async function next(iter) {
     })
 }
 
-const indexNames = ['doc-detail', 'id-mapper', 'reverse_index'];
+async function scanDetail(type) {
+    const rocksdb = new RocksDB('/Users/yuankui/git/grace-editor/index.dat/' + type);
+
+    await new Promise(resolve => {
+        rocksdb.open(async err => {
+            console.log('');
+            console.log(`=================index name [${type}]====================`);
+            console.log('');
+            const iterator = rocksdb.iterator();
+            for (let i = 0; i < 1000; i++) {
+                const [key, value] = await next(iterator);
+
+                if (key == null && value == null) {
+                    break;
+                }
+                console.log(`${key} => ${value}`)
+            }
+            resolve();
+        });
+    })
+}
+
+async function scanBitmap(type) {
+    const rocksdb = new RocksDB('/Users/yuankui/git/grace-editor/index.dat/' + type);
+
+    await new Promise(resolve => {
+        rocksdb.open(async err => {
+            console.log('');
+            console.log(`=================index name [${type}]====================`);
+            console.log('');
+            const iterator = rocksdb.iterator();
+            for (let i = 0; i < 1000; i++) {
+                const [key, value] = await next(iterator);
+
+                if (key == null && value == null) {
+                    break;
+                }
+
+                const bitmap = RoaringBitmap32.deserialize(value, true);
+                console.log(`${key} => ${JSON.stringify(bitmap.toArray())}`)
+            }
+            resolve();
+        });
+    })
+}
 
 async function run() {
-    for (let indexName of indexNames) {
-
-        const rocksdb = new RocksDB('/Users/yuankui/git/grace-editor/index.dat/' + indexName);
-
-        await new Promise(resolve => {
-            rocksdb.open(async err => {
-                console.log('');
-                console.log(`=================index name [${indexName}]====================`);
-                console.log('');
-                const iterator = rocksdb.iterator();
-                for (let i = 0; i < 1000; i++) {
-                    const [key, value] = await next(iterator);
-
-                    if (key == null && value == null) {
-                        break;
-                    }
-                    console.log(`${key} => ${value}`)
-                }
-                resolve();
-            });
-        })
-
-    }
+    await scanDetail('doc-detail');
+    await scanDetail('id-mapper');
+    await scanBitmap('reverse_index');
 }
 
 
-run().then(() => {
-});
+run().then(value => {});
