3343ec0431eaeac1fac86fee4dcece83db00cf6b
yuankui <yuankui64@gmail.com>
2020-04-21 21:32:02 +0800

    feature: support image move

--- src/app/renders/Slatejs/plugins/image/ScaleImage.tsx
+++ src/app/renders/Slatejs/plugins/image/ScaleImage.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties, FC, useEffect, useState} from "react";
+import React, {MouseEvent, CSSProperties, FC, useEffect, useState} from "react";
 
 interface Props {
     src: string,
@@ -23,17 +23,57 @@ const ScaleImage: FC<Props> = (props) => {
             }
 
             console.log(e);
-        }
-        window.addEventListener('wheel', onScroll)
+        };
+        window.addEventListener('wheel', onScroll);
         return () => {
             window.removeEventListener('wheel', onScroll);
         }
-    }, [scale])
+    }, [scale]);
+
+    const [pos, setPos] = useState([0, 0]);
+
+    const [mouseStartPos, setMouseStartPos] = useState([0, 0]);
+    const [mouseCurrentPos, setMouseCurrentPos] = useState([0, 0]);
+    const [moving, setMoving] = useState(false);
+
+    const onMouseDown = (e: MouseEvent) => {
+        setMoving(true);
+        setMouseCurrentPos([e.clientX, e.clientY]);
+        setMouseStartPos([e.clientX, e.clientY]);
+    };
+
+    const onMouseMoving = (e: MouseEvent) => {
+        setMouseCurrentPos([e.clientX, e.clientY]);
+    };
+
+    const onMouseUp = (e: MouseEvent) => {
+
+        const [oldX, oldY] = pos;
+        const [startX, startY] = mouseStartPos;
+        const [currentX, currentY] = mouseCurrentPos;
+
+        setMoving(false);
+        setPos([oldX + currentX - startX, oldY + currentY - startY]);
+    };
+
+    const [startX, startY] = mouseStartPos;
+    const [currentX, currentY] = mouseCurrentPos;
+    const [x, y] = pos;
+
+    const [xx, yy] = moving?[x + currentX - startX, y + currentY - startY] : pos;
 
     const scaleStyle: CSSProperties = {
         transform: `scale(${scale})`,
-    }
-    return <img src={props.src} style={scaleStyle}>
+        position: 'relative',
+        top: `${yy}px`,
+        left: `${xx}px`,
+    };
+    return <img src={props.src}
+                draggable={false}
+                style={scaleStyle}
+                onMouseDown={onMouseDown}
+                onMouseMove={onMouseMoving}
+                onMouseUp={onMouseUp}>
     </img>;
 };
 
