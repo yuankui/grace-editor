0f5a6e8a97b0abb8fe6d5067c1161545c2014669
yuankui <yuankui64@gmail.com>
2020-04-23 23:31:00 +0800

    up

--- src/app/tour/Tour.tsx
+++ src/app/tour/Tour.tsx
@@ -1,39 +1,38 @@
 import React, {FunctionComponent} from 'react';
 import Joyride, {CallBackProps, Props as TourProps} from 'react-joyride';
 import {TourTooltip} from "./TourTooltip";
+import {useExtensionManager} from "../../globalPlugins/useExtensionManager";
 
 interface Props {}
 
 
 const Tour: FunctionComponent<Props> = (props) => {
-    const state: TourProps = {
-        steps: [
-            {
-                target: 'body',
-                content: '接下来我们来浏览下大致功能吧！',
-            },
-            {
-                target: '.left-container',
-                content: '这里左边栏，文档列表和收藏都在这里',
-            },
-            {
-                target: '.app-left-handle',
-                content: '这里是空白，方便拖动窗口',
-            },
-            {
-                target: '.app-right-side',
-                content: '这是文档显示的地方',
-            },
-            {
-                target: '.show-setting',
-                content: '单击这里，可以查看设置',
-            },
-            {
-                target: '.create-new-btn',
-                content: '恭喜，界面了解基本完成。接下来单击这里新建一篇文章吧！',
-            },
-        ]
-    };
+    const steps = [
+        {
+            target: 'body',
+            content: '接下来我们来浏览下大致功能吧！',
+        },
+        {
+            target: '.left-container',
+            content: '这里左边栏，文档列表和收藏都在这里',
+        },
+        {
+            target: '.app-left-handle',
+            content: '这里是空白，方便拖动窗口',
+        },
+        {
+            target: '.app-right-side',
+            content: '这是文档显示的地方',
+        },
+        {
+            target: '.show-setting',
+            content: '单击这里，可以查看设置',
+        },
+        {
+            target: '.create-new-btn',
+            content: '恭喜，界面了解基本完成。接下来单击这里新建一篇文章吧！',
+        },
+    ];
 
     const styleOptions = {
         arrowColor: 'white',
@@ -45,16 +44,22 @@ const Tour: FunctionComponent<Props> = (props) => {
         width: undefined,
         zIndex: 100,
     };
+    const manager = useExtensionManager();
+    const done = manager.getSetting('core.tour', 'done');
+
     // 处理回调
     const callback = (data: CallBackProps) => {
-        // 处理点击设置按钮的时候，弹出设置
-        // if (data.lifecycle == 'complete' && data.step.target == ".show-setting") {
-        //     const settingButton = document.getElementsByClassName("show-setting")[0] as HTMLElement;
-        //     if (settingButton) {
-        //         settingButton.click();
-        //     }
-        // }
+        if (data.lifecycle == 'complete' && data.index == steps.length - 1) {
+            manager.setSetting('core.tour', 'done', true);
+        }
     };
+
+
+
+    // 如果已经看过了，就不展示了
+    if (done) {
+        return null;
+    }
     return <Joyride showProgress={true}
                     disableScrolling={true}
                     continuous={true}
@@ -63,7 +68,7 @@ const Tour: FunctionComponent<Props> = (props) => {
                     styles={{
                         options: styleOptions,
                     }}
-                    steps={state.steps} />;
+                    steps={steps} />;
 };
 
 export default Tour;
