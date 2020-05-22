24cd22fac55c59f00207afa12eb1a188d0acfa1b
yuankui <yuankui64@gmail.com>
2020-05-22 10:40:39 +0800

    add useMeno to improve performance

--- src/globalPlugins/PluginContextProvider.tsx
+++ src/globalPlugins/PluginContextProvider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {ExtensionManager} from "./Extension";
 import {createInternalPlugin} from "./createInteralPlugin";
 import {useStore} from "react-redux";
@@ -6,9 +6,12 @@ import {useStore} from "react-redux";
 export const ReactPluginContext = React.createContext<ExtensionManager>(null as any);
 
 export const PluginContextProvider: React.FC = props => {
-    let plugins = createInternalPlugin();
     let store = useStore();
-    let pluginContext = new ExtensionManager(plugins, store);
+
+    const pluginContext = useMemo(() => {
+        let plugins = createInternalPlugin();
+        return new ExtensionManager(plugins, store);
+    }, [])
 
     return <ReactPluginContext.Provider value={pluginContext}>
         {props.children}
