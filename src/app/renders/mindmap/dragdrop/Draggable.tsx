14783d4640d623bf6bae902a84b2541da4b63751
yuankui <yuankui64@gmail.com>
2020-05-26 16:19:26 +0800

    up

---
+++ src/app/renders/mindmap/dragdrop/Draggable.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {}
+
+const Draggable: FunctionComponent<Props> = (props) => {
+    return <>
+
+    </>;
+};
+
+export default Draggable;
