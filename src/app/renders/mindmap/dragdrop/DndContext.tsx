44b1a7d3ff6ba9f318d803f536a8283e19586b37
yuankui <yuankui64@gmail.com>
2020-06-04 17:49:53 +0800

    up

--- src/app/renders/mindmap/dragdrop/DndContext.tsx
+++ src/app/renders/mindmap/dragdrop/DndContext.tsx
@@ -2,9 +2,24 @@ import {FunctionComponent, useContext, useState} from 'react';
 import React from "react";
 
 export interface DndState {
+    /**
+     * 源元素的对象表示，比如一个article列表的一项就可以用这个列表代表的Article对象表示
+     */
     src: any,
+
+    /**
+     * 是否在移动
+     */
     moving: boolean,
+
+    /**
+     * 开始移动的点
+     */
     startPoint?: Point,
+
+    /**
+     * 当前点
+     */
     currentPoint?: Point,
 }
 
