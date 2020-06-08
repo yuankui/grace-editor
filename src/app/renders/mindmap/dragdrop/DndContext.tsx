351787d4779af2450aba23e9e419b60aeaa0006d
yuankui <yuankui64@gmail.com>
2020-06-08 15:32:34 +0800

    optimize imports

--- src/app/renders/mindmap/dragdrop/DndContext.tsx
+++ src/app/renders/mindmap/dragdrop/DndContext.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent, useContext, useEffect, useMemo, useState} from 'react';
 import {Observable} from 'rxjs';
-import {sample, sampleTime} from "rxjs/operators";
+import {sample} from "rxjs/operators";
 import mitt from 'mitt';
 
 export interface DndState {
