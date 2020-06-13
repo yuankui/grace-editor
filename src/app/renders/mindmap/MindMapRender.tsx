c02fa5781ef814f8ebca8c55f4a1907730c61da7
yuankui <yuankui64@gmail.com>
2020-06-13 19:07:41 +0800

    up

--- src/app/renders/mindmap/MindMapRender.tsx
+++ src/app/renders/mindmap/MindMapRender.tsx
@@ -1,9 +1,44 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useEffect, useMemo, useRef, useState} from 'react';
 import {RenderProps} from "../renders";
-import MindMap from "./v2/MindMap";
+import MindMap from "./MindMap";
+import {Value} from "./model";
+import {lazyExecute} from "../../../utils/lazyExecute";
+import {createEmptyNode} from "./createEmptyNode";
+
+const defaultValue: Value = {
+    roots: [
+        createEmptyNode(),
+    ],
+    pos: [],
+    scale: 1,
+    origin: {
+        x: 500,
+        y: 250,
+    }
+};
 
 const MindMapRender: FunctionComponent<RenderProps> = (props) => {
-    return <MindMap />;
+    const {
+        onChange,
+        value,
+        readOnly,
+    } = props;
+
+    const [state, setState] = useState<Value>(value || defaultValue);
+
+    const lazySave = useMemo(() => {
+        const lazySave = lazyExecute(onChange, 500);
+        return (value: Value) => {
+            setState(value);
+            lazySave(value);
+        };
+    }, [])
+
+
+    return <MindMap value={state}
+                    height={'100%'}
+                    width={'100%'}
+                    onChange={lazySave}/>;
 };
 
 export default MindMapRender;
